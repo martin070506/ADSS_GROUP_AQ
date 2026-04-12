e93e6c72f83d998548eda77b05e8a4188eec904a
Liav Parhi <liavparhi111@gmail.com>
2026-04-12 19:58:00 +0300

    Packages

--- dev/hw0/src/Domain/Product.java
+++ dev/hw0/src/Domain/Product.java
@@ -5,12 +5,12 @@ public class Product {
     private String name;
     private int weight; // Kg
 
-    Product(String name, int weight) {
+    public Product(String name, int weight) {
         this.name = name;
         this.weight = weight;
     }
 
-    public int getName() {
+    public String getName() {
         return name;
     }
     public int getWeight() {
